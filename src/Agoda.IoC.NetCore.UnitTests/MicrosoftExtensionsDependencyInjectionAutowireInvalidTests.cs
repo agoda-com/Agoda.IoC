d938e8ef0e67d7242f9d832855f5715a33ea2171
Keattisak Chinburarat <keattisak.chinburarat@agoda.com>
2021-02-16 12:33:48 +0700

    add test

---
+++ src/Agoda.IoC.NetCore.UnitTests/MicrosoftExtensionsDependencyInjectionAutowireInvalidTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Agoda.IoC.Core;
+using Agoda.IoC.ProjectUnderTest.Invalid12;
+using Agoda.IoC.ProjectUnderTest.Valid;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using Shouldly;
+
+namespace Agoda.IoC.NetCore.UnitTests
+{
+    [TestFixture]
+    public class MicrosoftExtensionsDependencyInjectionAutowireInvalidTests
+    {
+        [Test]
+        public void RegisterByAttribute_WithAmbiguousRegistration_Throws()
+        {
+            var container = new ServiceCollection();
+            Assert.Throws<ContainerRegistrationContextException>(() => container.AutoWireAssembly(new[]
+            {
+                typeof(AmbiguousRegistration).Assembly
+            }, false,onexceptio => {
+                onexceptio.Ignore = false;
+            }));
+        }
+    }
+}
